aa4bdc3e14e641467994b48a1ad9b00ed8418e41
reallyalien <reallyalien@yeah.net>
2020-05-19 17:00:00 +0800

    登录组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Login from '../views/login';
 //extensions: ['.js', '.vue', '.json'],扩展名称，默认会按照这个顺序寻找，所以这里省略了后缀名称
 
 Vue.use(Router);
 
-import Login from '../views/login';
 export default new Router({
   routes: [
-
+    {name:'login',path:'/login',component:Login},
   ]
 });
